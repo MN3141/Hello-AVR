48f13fcf5a78f28096ac7a0bb0f1acf689e80311
Andrei Saldorfean <andreisaldorfean@gmail.com>
2025-05-14 18:38:05 +0300

    fix: Fix bugs that prevented flashing
    Signed-off-by: Andrei Saldorfean <andreisaldorfean@gmail.com>

--- workspace/cmake/src/main.c
+++ workspace/cmake/src/main.c
@@ -1,10 +1,12 @@
 #include <avr/io.h>
-#include <avr/iom328p.h>
+#include <util/delay.h>
 
-int main(){
-
-  while(1){
-
-  }
-  return 0;
+int main() {
+    DDRB = 0xFF; // Set PORTB as output
+    PORTB = 0x00; // Initialize PORTB to 0
+    while (1) {
+        PORTB ^= 0xFF;
+        _delay_ms(1000);
+    }
+    return 0;
 }
